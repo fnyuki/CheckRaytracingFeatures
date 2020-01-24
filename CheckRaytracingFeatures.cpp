f02e235e93740c87e60e43555180f1aa7aa76fd0
Yuki Kawana <ky.86623@konami.com>
2020-01-24 13:25:47 +0900

    d3d12 minimal

--- CheckRaytracingFeatures.cpp
+++ CheckRaytracingFeatures.cpp
@@ -1,20 +1,16 @@
-﻿// CheckRaytracingFeatures.cpp : このファイルには 'main' 関数が含まれています。プログラム実行の開始と終了がそこで行われます。
-//
-
-#include <iostream>
+﻿#include "windows.h"
+#include "d3d12.h"
+#include "dxgi1_6.h"
+#include "string"
+#include "unordered_map"
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	auto hr = 0;
+	// UINT dxgiFactoryFlags = 0;
+	IDXGIFactory7* factory = nullptr;
+	IDXGIAdapter1* adapter = nullptr;
+	ID3D12Device6* device = nullptr;
+	CreateDXGIFactory2(0, IID_PPV_ARGS(&factory));
+	factory->Release();
 }
-
-// プログラムの実行: Ctrl + F5 または [デバッグ] > [デバッグなしで開始] メニュー
-// プログラムのデバッグ: F5 または [デバッグ] > [デバッグの開始] メニュー
-
-// 作業を開始するためのヒント: 
-//    1. ソリューション エクスプローラー ウィンドウを使用してファイルを追加/管理します 
-//   2. チーム エクスプローラー ウィンドウを使用してソース管理に接続します
-//   3. 出力ウィンドウを使用して、ビルド出力とその他のメッセージを表示します
-//   4. エラー一覧ウィンドウを使用してエラーを表示します
-//   5. [プロジェクト] > [新しい項目の追加] と移動して新しいコード ファイルを作成するか、[プロジェクト] > [既存の項目の追加] と移動して既存のコード ファイルをプロジェクトに追加します
-//   6. 後ほどこのプロジェクトを再び開く場合、[ファイル] > [開く] > [プロジェクト] と移動して .sln ファイルを選択します
