6995195136cb02e1c00fe53ea7b8f4bcfb20925c
Yuki Kawana <ky.86623@konami.com>
2020-01-24 14:37:38 +0900

    src 公開

--- src/CheckRaytracingFeatures.cpp
+++ src/CheckRaytracingFeatures.cpp
@@ -1,16 +1,92 @@
-﻿#include "windows.h"
-#include "d3d12.h"
-#include "dxgi1_6.h"
-#include "string"
-#include "unordered_map"
+﻿#include <string>
 
-int main()
+#include "CheckRaytracingFeatures.h"
+
+int main(int argc, const char* argv[])
 {
-	auto hr = 0;
-	// UINT dxgiFactoryFlags = 0;
-	IDXGIFactory7* factory = nullptr;
-	IDXGIAdapter1* adapter = nullptr;
-	ID3D12Device6* device = nullptr;
-	CreateDXGIFactory2(0, IID_PPV_ARGS(&factory));
-	factory->Release();
+	HRESULT								hr			= 0;
+	IDXGIFactory7*						factory		= nullptr;
+	IDXGIAdapter1*						adapter		= nullptr;
+	ID3D12Device6*						device		= nullptr;
+	D3D12_FEATURE_DATA_D3D12_OPTIONS5	featureData	= {};
+
+	if (!CreateFactory(hr, factory))
+	{
+		goto DISPOSE;
+	}
+
+	if (!CreateAdapter(factory, hr, adapter))
+	{
+		goto DISPOSE;
+	}
+
+	if (!CreateDevice(adapter, hr, device))
+	{
+		goto DISPOSE;
+	}
+
+	if (!CheckRaytracingFeatureSupport(device, hr, featureData))
+	{
+		goto DISPOSE;
+	}
+
+DISPOSE:
+	SafeRelease(device);
+	SafeRelease(adapter);
+	SafeRelease(factory);
+
+	if (SUCCEEDED(hr))
+	{
+		std::string tierMessage;
+		tierMessage += "Raytracing tier: ";
+		tierMessage += GetRaytracingTierString(featureData.RaytracingTier);
+		ShowDialog("Success", tierMessage);
+	}
+	else
+	{
+		std::string errorMessage;
+		errorMessage += "An error was occured.\n";
+		errorMessage += GetErrorMessage(hr);
+		ShowDialog("Error", errorMessage);
+	}
+
+	return (int)hr;
+}
+
+bool CreateFactory(HRESULT& hr, IDXGIFactory7*& factory)
+{
+	hr = CreateDXGIFactory2(0, IID_PPV_ARGS(&factory));
+	return SUCCEEDED(hr);
+}
+
+bool CreateAdapter(IDXGIFactory7* const factory, HRESULT& hr, IDXGIAdapter1*& adapter)
+{
+	DXGI_ADAPTER_DESC1 desc = {};
+	for (auto adapterIndex = 0; factory->EnumAdapters1(adapterIndex, &adapter) != DXGI_ERROR_NOT_FOUND; ++adapterIndex)
+	{
+		adapter->GetDesc1(&desc);
+		if (desc.Flags & DXGI_ADAPTER_FLAG_SOFTWARE)
+		{
+			continue;
+		}
+		hr = D3D12CreateDevice(adapter, D3D_FEATURE_LEVEL_12_0, __uuidof(ID3D12Device), nullptr);
+		if (SUCCEEDED(hr))
+		{
+			return true;
+		}
+		SafeRelease(adapter);
+	}
+	return false;
+}
+
+bool CreateDevice(IDXGIAdapter1* const adapter, HRESULT& hr, ID3D12Device6*& device)
+{
+	hr = D3D12CreateDevice(adapter, D3D_FEATURE_LEVEL_12_0, IID_PPV_ARGS(&device));
+	return SUCCEEDED(hr);
+}
+
+bool CheckRaytracingFeatureSupport(ID3D12Device6* const device, HRESULT& hr, D3D12_FEATURE_DATA_D3D12_OPTIONS5& featureData)
+{
+	hr = device->CheckFeatureSupport(D3D12_FEATURE_D3D12_OPTIONS5, &featureData, sizeof(featureData));
+	return SUCCEEDED(hr);
 }
--- src/CheckRaytracingFeatures.h
+++ src/CheckRaytracingFeatures.h
@@ -1,1 +1,54 @@
 #pragma once
+
+#define SHOW_MESSAGE_AS_DIALOG 0
+
+#include <windows.h>
+#include <comdef.h>
+#include <d3d12.h>
+#include <dxgi1_6.h>
+#include <string_view>
+#include <type_traits>
+#if !SHOW_MESSAGE_AS_DIALOG
+#	include <iostream>
+#endif
+
+template <class T>
+void SafeRelease(T*& p)
+{
+	static_assert(std::is_base_of_v<IUnknown, T>);
+	if (p != nullptr)
+	{
+		p->Release();
+		p = nullptr;
+	}
+}
+
+inline void ShowDialog(const std::string_view title, const std::string_view detail)
+{
+#if !SHOW_MESSAGE_AS_DIALOG
+	std::cout << title.data() << ": " << detail.data() << std::endl;
+#else
+	MessageBox(nullptr, detail.data(), title.data(), 0);
+#endif
+}
+
+inline const char* GetErrorMessage(const HRESULT hr)
+{
+	return _com_error(hr).ErrorMessage();
+}
+
+constexpr const char* GetRaytracingTierString(const D3D12_RAYTRACING_TIER tier)
+{
+	return tier == D3D12_RAYTRACING_TIER_NOT_SUPPORTED	? "Not supported" :
+	       tier == D3D12_RAYTRACING_TIER_1_0			? "1.0" :
+	       tier == D3D12_RAYTRACING_TIER_1_1			? "1.1" :
+	       "Not found";
+};
+
+bool CreateFactory(HRESULT& hr, IDXGIFactory7*& factory);
+
+bool CreateAdapter(IDXGIFactory7* const factory, HRESULT& hr, IDXGIAdapter1*& adapter);
+
+bool CreateDevice(IDXGIAdapter1* const adapter, HRESULT& hr, ID3D12Device6*& device);
+
+bool CheckRaytracingFeatureSupport(ID3D12Device6* const device, HRESULT& hr, D3D12_FEATURE_DATA_D3D12_OPTIONS5& featureData);
